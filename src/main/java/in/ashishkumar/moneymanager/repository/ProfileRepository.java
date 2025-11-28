57b9cbdd53c8aeb26d13eb99e5bb8090762fbf18
Ashish kumar <kr3164ashish@gmail.com>
2025-11-28 12:58:18 +0530

    initial commit

---
+++ src/main/java/in/ashishkumar/moneymanager/repository/ProfileRepository.java
@@ -0,0 +1,14 @@
+package in.ashishkumar.moneymanager.repository;
+
+import in.ashishkumar.moneymanager.entity.ProfileEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ProfileRepository extends JpaRepository<ProfileEntity, Long> {
+
+    //select * from tbl_profiles where email = ?
+    Optional<ProfileEntity> findByEmail(String email);
+    //select * from tbl_profiles where activation_token = ?
+    Optional<ProfileEntity> findByActivationToken(String activationToken);
+}
